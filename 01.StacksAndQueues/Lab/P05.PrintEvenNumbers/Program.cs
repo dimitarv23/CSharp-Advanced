bf2a85d94aab108ea46dd2f740b5cee3df23a644
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-10 12:35:49 +0300

    Added P05

---
+++ 01.StacksAndQueues/Lab/P05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> evenNumbers = new Queue<int>();
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evenNumbers.Enqueue(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}
