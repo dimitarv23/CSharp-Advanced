ec540edaa8c3ea5b6d1212686d353842e29bec08
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-10 12:35:56 +0300

    Added P06

---
+++ 01.StacksAndQueues/Lab/P06.Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06.Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+
+            string name = Console.ReadLine();
+            while (name != "End")
+            {
+                if (name == "Paid")
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, names));
+                    names.Clear();
+                }
+                else
+                {
+                    names.Enqueue(name);
+                }
+                
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
