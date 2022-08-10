533742a8ee7eda0d3422cc1e0033886d0e459ec0
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-10 12:36:02 +0300

    Added P07

---
+++ 01.StacksAndQueues/Lab/P07.HotPotato/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> kids = new Queue<string>(names);
+
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            while (kids.Count > 1)
+            {
+                string currKid = kids.Dequeue();
+                counter++;
+
+                if (counter == n)
+                {
+                    Console.WriteLine($"Removed {currKid}");
+                    counter = 0;
+                }
+                else
+                {
+                    kids.Enqueue(currKid);
+                }
+            }
+
+            Console.WriteLine($"Last is {kids.Peek()}");
+        }
+    }
+}
