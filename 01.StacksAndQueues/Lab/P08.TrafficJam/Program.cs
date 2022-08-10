ea93f76611c7035c2f297d4f3442134750b92db3
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-10 12:36:09 +0300

    Added P08

---
+++ 01.StacksAndQueues/Lab/P08.TrafficJam/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P08.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> cars = new Queue<string>();
+            int n = int.Parse(Console.ReadLine());
+            int counterPassed = 0;
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < n && cars.Count > 0; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        counterPassed++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{counterPassed} cars passed the crossroads.");
+        }
+    }
+}
