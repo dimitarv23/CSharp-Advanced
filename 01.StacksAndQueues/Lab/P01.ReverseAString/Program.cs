abd9a4f2915db40ad9bedd4e1ef9a3ce141a9cf3
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-10 12:35:17 +0300

    Added P01

---
+++ 01.StacksAndQueues/Lab/P01.ReverseAString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                stack.Push(c);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
