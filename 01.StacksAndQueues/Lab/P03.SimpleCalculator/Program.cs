b9440612b16c973a9d6f8f520f53ffb0777be7f1
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-10 12:35:37 +0300

    Added P03

---
+++ 01.StacksAndQueues/Lab/P03.SimpleCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P03.SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Stack<string> stack = new Stack<string>(input.Reverse());
+
+            while (stack.Count > 1)
+            {
+                int firstNum = int.Parse(stack.Pop());
+                string action = stack.Pop();
+                int secondNum = int.Parse(stack.Pop());
+
+                if (action == "+")
+                {
+                    stack.Push((firstNum + secondNum).ToString());
+                }
+                else if (action == "-")
+                {
+                    stack.Push((firstNum - secondNum).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
