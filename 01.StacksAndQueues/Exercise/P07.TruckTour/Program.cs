363efa64da07cef40a8cd311855e6fea49b51e56
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-11 15:25:19 +0300

    Added P07

---
+++ 01.StacksAndQueues/Exercise/P07.TruckTour/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.TruckTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int[]> queue = new Queue<int[]>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] petrolPumpInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                queue.Enqueue(petrolPumpInfo);
+            }
+
+            int startIndex = 0;
+
+            while (true)
+            {
+                int totalLiters = 0;
+                bool isComplete = true;
+
+                foreach (var item in queue)
+                {
+                    int liters = item[0];
+                    int distance = item[1];
+
+                    totalLiters += liters;
+
+                    if (totalLiters < distance)
+                    {
+                        startIndex++;
+                        int[] currPump = queue.Dequeue();
+                        queue.Enqueue(currPump);
+                        isComplete = false;
+                        break;
+                    }
+
+                    totalLiters -= distance;
+                }
+
+                if (isComplete)
+                {
+                    Console.WriteLine(startIndex);
+                    break;
+                }
+            }
+        }
+    }
+}
