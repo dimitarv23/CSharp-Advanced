bb12bb9505c7a04610d63362f1369740c7a508f9
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-11 15:25:02 +0300

    Added P04

---
+++ 01.StacksAndQueues/Exercise/P04.FastFood/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+
+            int[] orders = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(orders.Max());
+
+            Queue<int> ordersQueue = new Queue<int>(orders);
+
+            while (ordersQueue.Count > 0)
+            {
+                if (foodQuantity >= ordersQueue.Peek())
+                {
+                    foodQuantity -= ordersQueue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (ordersQueue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", ordersQueue)}");
+            }
+        }
+    }
+}
