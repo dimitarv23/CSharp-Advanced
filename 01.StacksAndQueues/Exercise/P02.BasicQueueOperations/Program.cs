8cb4191294528c473b79b3cde156bb39c8af742e
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-11 15:24:50 +0300

    Added P02

---
+++ 01.StacksAndQueues/Exercise/P02.BasicQueueOperations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P02.BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stackInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int elementsToDequeue = stackInfo[1];
+            int searchedElement = stackInfo[2];
+
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < elementsToDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (queue.Contains(searchedElement))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+        }
+    }
+}
