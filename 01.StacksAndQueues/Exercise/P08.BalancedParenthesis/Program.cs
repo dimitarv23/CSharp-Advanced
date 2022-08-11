1ed80c8ae926800c01ba9f080e9259c532750554
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-11 15:25:24 +0300

    Added P08

---
+++ 01.StacksAndQueues/Exercise/P08.BalancedParenthesis/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P08.BalancedParenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            bool isCorrect = true;
+
+            foreach (char bracket in expression)
+            {
+                if (bracket == '(' || bracket == '[' || bracket == '{')
+                {
+                    stack.Push(bracket);
+                }
+                else if (bracket == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+
+                    if (stack.Peek() == '(')
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (bracket == ']')
+                {
+                    if (stack.Count == 0)
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+
+                    if (stack.Peek() == '[')
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (bracket == '}')
+                {
+                    if (stack.Count == 0)
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+
+                    if (stack.Peek() == '{')
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+
+            if (stack.Count > 0 || isCorrect == false)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
