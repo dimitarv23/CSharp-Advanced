8792b79e2c0f68db075fe9855d7611e07e938bb2
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-11 15:24:42 +0300

    Added P01

---
+++ 01.StacksAndQueues/Exercise/P01.BasicStackOperations/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+
+namespace P01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stackInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int elementsToPop = stackInfo[1];
+            int searchedElement = stackInfo[2];
+
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+
+            for (int i = 0; i < elementsToPop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (stack.Contains(searchedElement))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+        }
+    }
+}
