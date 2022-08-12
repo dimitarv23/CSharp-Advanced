1542ac078ae7e9da67066f4146e32b6178756259
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-12 17:12:04 +0300

    Added P02

---
+++ 02.Multidimensional Arrays/Lab/P02.SumMatrixColumns/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currRow = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int currColSum = 0;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    currColSum += matrix[row, col];
+                }
+
+                Console.WriteLine(currColSum);
+            }
+        }
+    }
+}
