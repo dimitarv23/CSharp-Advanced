41f2b0cc66c971341046bebc2593892307909176
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-12 17:12:10 +0300

    Added P03

---
+++ 02.Multidimensional Arrays/Lab/P03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            int sumPrimaryDiagonal = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currRow = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                sumPrimaryDiagonal += matrix[row, row];
+            }
+
+            Console.WriteLine(sumPrimaryDiagonal);
+        }
+    }
+}
