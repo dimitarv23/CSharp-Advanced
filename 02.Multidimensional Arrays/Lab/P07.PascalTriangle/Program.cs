df74a04c6c276e33c662c0c8036e18db4e77d8ff
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-12 17:12:35 +0300

    Added P07

---
+++ 02.Multidimensional Arrays/Lab/P07.PascalTriangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P07.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            long[][] jagged = new long[n][];
+            jagged[0] = new long[1];
+            jagged[0][0] = 1;
+
+            for (int row = 1; row < jagged.Length; row++)
+            {
+                jagged[row] = new long[row + 1];
+                jagged[row][0] = 1;
+
+                for (int col = 1; col < jagged[row].Length; col++)
+                {
+                    jagged[row][col] = jagged[row - 1][col - 1];
+
+                    if (col < jagged[row - 1].Length)
+                    {
+                        jagged[row][col] += jagged[row - 1][col];
+                    }
+                }
+            }
+
+            PrintJagged(jagged);
+        }
+
+        static void PrintJagged(long[][] jagged)
+        {
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                Console.WriteLine(string.Join(" ", jagged[row]));
+            }
+        }
+    }
+}
