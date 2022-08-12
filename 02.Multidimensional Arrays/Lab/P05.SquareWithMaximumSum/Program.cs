a0b0352b112ec8fe025af56517407220db108c9b
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-12 17:12:24 +0300

    Added P05

---
+++ 02.Multidimensional Arrays/Lab/P05.SquareWithMaximumSum/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace P05.SquareWithMaximumSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            int[,] matrix = new int[rows, cols];
+            ReadMatrix(matrix);
+
+            int maxSquareSum = int.MinValue;
+            int maxSquareRow = 0;
+            int maxSquareCol = 0;
+            int currSquareSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    currSquareSum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    if (currSquareSum > maxSquareSum)
+                    {
+                        maxSquareSum = currSquareSum;
+                        maxSquareRow = row;
+                        maxSquareCol = col;
+                    }
+                }
+            }
+
+            for (int row = maxSquareRow; row <= maxSquareRow + 1; row++)
+            {
+                for (int col = maxSquareCol; col <= maxSquareCol + 1; col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(maxSquareSum);
+        }
+
+        static void ReadMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currRow = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+        }
+    }
+}
