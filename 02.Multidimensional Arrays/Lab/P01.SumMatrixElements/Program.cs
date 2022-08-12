3fea534533bc124969f9287728d35252cf81a852
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-12 17:11:59 +0300

    Added P01

---
+++ 02.Multidimensional Arrays/Lab/P01.SumMatrixElements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            int[,] matrix = new int[rows, cols];
+            int totalSum = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currRow = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currRow[col];
+                    totalSum += currRow[col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(totalSum);
+        }
+    }
+}
