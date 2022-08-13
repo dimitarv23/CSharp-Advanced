1d63c08503752bce5140cf9018fce1276de3305c
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-13 16:07:27 +0300

    Added P06

---
+++ 02.Multidimensional Arrays/Exercise/P06.JaggedArrayManipulator/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace P06.JaggedArrayManipulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] jagged = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] currRow = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                jagged[i] = currRow;
+            }
+
+            for (int row = 0; row < jagged.Length - 1; row++)
+            {
+                if (jagged[row].Length == jagged[row + 1].Length)
+                {
+                    for (int col = 0; col < jagged[row].Length; col++)
+                    {
+                        jagged[row][col] *= 2;
+                        jagged[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < jagged[row].Length; col++)
+                    {
+                        jagged[row][col] /= 2;
+                    }
+
+                    for (int col = 0; col < jagged[row + 1].Length; col++)
+                    {
+                        jagged[row + 1][col] /= 2;
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] cmdArgs = command.Split();
+                string action = cmdArgs[0];
+                int row = int.Parse(cmdArgs[1]);
+                int col = int.Parse(cmdArgs[2]);
+                int value = int.Parse(cmdArgs[3]);
+
+                if (!AreIndicesValid(jagged, row, col))
+                {
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                if (action == "Add")
+                {
+                    jagged[row][col] += value;
+                }
+                else if (action == "Subtract")
+                {
+                    jagged[row][col] -= value;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                Console.WriteLine(string.Join(" ", jagged[row]));
+            }
+        }
+
+        static bool AreIndicesValid(int[][] jagged, int rowIndex, int colIndex)
+        {
+            if (rowIndex < 0 || rowIndex >= jagged.Length)
+            {
+                return false;
+            }
+
+            if (colIndex < 0 || colIndex >= jagged[rowIndex].Length)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
