5eacb3e105c2ea9593a98260fe6981e5685605f6
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-13 16:06:56 +0300

    Added P01

---
+++ 02.Multidimensional Arrays/Exercise/P01.DiagonalDifference/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            ReadMatrix(matrix);
+
+            for (int i = 0; i < n; i++)
+            {
+                primaryDiagonalSum += matrix[i, i];
+                secondaryDiagonalSum += matrix[i, n - i - 1];
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
+        }
+
+        static void ReadMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currRow = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+        }
+    }
+}
