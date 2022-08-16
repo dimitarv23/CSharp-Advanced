09a9d8e31e21c2195f89b6febc81aeb4920b9025
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-16 19:18:48 +0300

    Added P07

---
+++ 03.SetsAndDictionariesAdvanced/Lab/P07.ParkingLot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+namespace P07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] cmdArgs = command.Split(", ");
+                string action = cmdArgs[0];
+                string licensePlate = cmdArgs[1];
+
+                if (action == "IN")
+                {
+                    if (!cars.Contains(licensePlate))
+                    {
+                        cars.Add(licensePlate);
+                    }
+                }
+                else if (action == "OUT")
+                {
+                    if (cars.Contains(licensePlate))
+                    {
+                        cars.Remove(licensePlate);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(cars.Count > 0 ? string.Join(Environment.NewLine, cars) : "Parking Lot is Empty");
+        }
+    }
+}
