73fa5553d656f80cbc8ce8f535abab4fbd35de5e
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-16 19:18:14 +0300

    Added P01

---
+++ 03.SetsAndDictionariesAdvanced/Lab/P01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> occurencies = new Dictionary<double, int>();
+
+            double[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            foreach (var number in numbers)
+            {
+                if (occurencies.ContainsKey(number))
+                {
+                    occurencies[number]++;
+                }
+                else
+                {
+                    occurencies[number] = 1;
+                }
+            }
+
+            foreach (var occurence in occurencies)
+            {
+                Console.WriteLine($"{occurence.Key} - {occurence.Value} times");
+            }
+        }
+    }
+}
