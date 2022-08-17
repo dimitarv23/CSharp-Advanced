7a7ad87e2f203105ee1d1d75a2bd53bcd35039be
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-17 18:47:27 +0300

    Added P03

---
+++ 03.SetsAndDictionariesAdvanced/Exercise/P03.PeriodicTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> elements = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] chemicalCompounds = Console.ReadLine()
+                    .Split();
+
+                foreach (var element in chemicalCompounds)
+                {
+                    if (!elements.Contains(element))
+                    {
+                        elements.Add(element);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements.OrderBy(x => x)));
+        }
+    }
+}
