021e76d8d309804e72356fa4cb5e0b65b126db1f
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-17 18:47:40 +0300

    Added P05

---
+++ 03.SetsAndDictionariesAdvanced/Exercise/P05.CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> occurencies = new Dictionary<char, int>();
+            string input = Console.ReadLine();
+
+            foreach (var ch in input)
+            {
+                if (!occurencies.ContainsKey(ch))
+                {
+                    occurencies.Add(ch, 0);
+                }
+
+                occurencies[ch]++;
+            }
+
+            foreach (var occurence in occurencies.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{occurence.Key}: {occurence.Value} time/s");
+            }
+        }
+    }
+}
