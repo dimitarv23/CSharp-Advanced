ee4f2c22d19ca951c4165e119fb4795e88007b7a
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-17 18:47:33 +0300

    Added P04

---
+++ 03.SetsAndDictionariesAdvanced/Exercise/P04.EvenTimes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> occurencies = new Dictionary<int, int>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int currNum = int.Parse(Console.ReadLine());
+
+                if (!occurencies.ContainsKey(currNum))
+                {
+                    occurencies.Add(currNum, 0);
+                }
+
+                occurencies[currNum]++;
+            }
+
+            foreach (var occurence in occurencies)
+            {
+                if (occurence.Value % 2 == 0)
+                {
+                    Console.WriteLine(occurence.Key);
+                }
+            }
+        }
+    }
+}
