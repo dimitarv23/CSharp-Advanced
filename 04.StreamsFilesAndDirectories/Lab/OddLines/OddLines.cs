9bd7db4a899729068350525894153bf49cd20c81
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-23 19:22:06 +0300

    Added OddLines

---
+++ 04.StreamsFilesAndDirectories/Lab/OddLines/OddLines.cs
@@ -0,0 +1,39 @@
+﻿namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                int index = 0;
+
+                using (StreamReader reader = new StreamReader(inputFilePath))
+                {
+                    var line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        if (index % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        index++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
