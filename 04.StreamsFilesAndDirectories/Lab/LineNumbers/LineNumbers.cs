eb602d014be4d707d7b64eeb4609b2f70f60bef7
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-23 19:22:20 +0300

    Added LineNumbers

---
+++ 04.StreamsFilesAndDirectories/Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,34 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                using (StreamReader reader = new StreamReader(inputFilePath))
+                {
+                    var line = reader.ReadLine();
+                    int index = 1;
+
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{index}. {line}");
+                        index++;
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
