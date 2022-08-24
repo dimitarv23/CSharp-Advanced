39910dc0f1d147b4d431c5d092655813a1200192
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-24 15:54:27 +0300

    Added ExtractSpecialBytes

---
+++ 04.StreamsFilesAndDirectories/Lab/ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace ExtractSpecialBytes
+{
+    using System;
+    using System.IO;
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            var output = new StringBuilder();
+            byte[] binaryFileBytes = Encoding.UTF8.GetBytes(binaryFilePath);
+            string[] bytes = File.ReadAllLines(bytesFilePath);
+
+            using (var writer = new StreamWriter(outputPath))
+            {
+                foreach (var item in binaryFileBytes)
+                {
+                    if (bytes.Contains(item.ToString()))
+                    {
+                        output.Append(item.ToString());
+                    }
+                }
+            }
+
+            File.WriteAllText(outputPath, output.ToString());
+        }
+    }
+}
