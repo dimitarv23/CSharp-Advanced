a5a574b3f6d9b569c2a29f0f09bd29dadf486849
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-26 09:18:02 +0300

    Added EvenLines

---
+++ 04.StreamsFilesAndDirectories/Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Text;
+
+namespace EvenLines
+{
+    using System;
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder result = new StringBuilder();
+
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                string line = reader.ReadLine();
+                int counter = 0;
+
+                while (line != null)
+                {
+                    if (counter % 2 == 1)
+                    {
+                        line = reader.ReadLine();
+                        counter++;
+                        continue;
+                    }
+
+                    string[] lineWords = line
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    Array.Reverse(lineWords);
+
+                    for (int i = 0; i < lineWords.Length; i++)
+                    {
+                        lineWords[i] = lineWords[i].Replace('-', '@');
+                        lineWords[i] = lineWords[i].Replace(',', '@');
+                        lineWords[i] = lineWords[i].Replace('.', '@');
+                        lineWords[i] = lineWords[i].Replace('!', '@');
+                        lineWords[i] = lineWords[i].Replace('?', '@');
+
+                        result.Append($"{lineWords[i]}");
+
+                        if (i < lineWords.Length - 1)
+                        {
+                            result.Append(" ");
+                        }
+                    }
+
+
+                    counter++;
+                    line = reader.ReadLine();
+
+                    if (line != null)
+                    {
+                        result.Append(Environment.NewLine);
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
