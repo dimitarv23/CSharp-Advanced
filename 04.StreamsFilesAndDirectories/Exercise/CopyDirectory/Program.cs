cd3d06481ea989fff007dc592d0418b538ce638e
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-26 09:18:22 +0300

    Added CopyDirectory

---
+++ 04.StreamsFilesAndDirectories/Exercise/CopyDirectory/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace CopyDirectory
+{
+    using System;
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            Directory.Move(inputPath, outputPath);
+        }
+    }
+}
