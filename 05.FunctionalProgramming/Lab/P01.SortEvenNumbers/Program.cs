2f73a2a8064305d1fb821cd597db35464274c90e
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-01 21:45:35 +0300

    Added P01

---
+++ 05.FunctionalProgramming/Lab/P01.SortEvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] evenNumbersSorted = numbers
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", evenNumbersSorted));
+        }
+    }
+}
