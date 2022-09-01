2e462dfaa9d35b2963b0101458c106ec86651381
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-01 21:45:41 +0300

    Added P02

---
+++ 05.FunctionalProgramming/Lab/P02.SumNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> ParseElement = x => int.Parse(x);
+            Func<int[], int> CountElements = x => x.Length;
+            Func<int[], long> SumElements = x => x.Sum();
+
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(ParseElement)
+                .ToArray();
+
+            Console.WriteLine(CountElements(numbers));
+            Console.WriteLine(SumElements(numbers));
+        }
+    }
+}
