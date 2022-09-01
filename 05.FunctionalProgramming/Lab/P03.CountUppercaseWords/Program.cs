bf9d9d4b81d15de2a6e329cd46501fed7afc6a22
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-01 21:45:47 +0300

    Added P03

---
+++ 05.FunctionalProgramming/Lab/P03.CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> isUppercase = x => Char.IsUpper(x.First());
+
+            string[] uppercaseWords = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(isUppercase)
+                .ToArray();
+            
+            Console.WriteLine(string.Join(Environment.NewLine, uppercaseWords));
+        }
+    }
+}
