63d2e934045188a8461b64319713fa28f763bfdb
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-01 21:45:59 +0300

    Added P04

---
+++ 05.FunctionalProgramming/Lab/P04.AddVAT/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace P04.AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, double> ParseElement = x => double.Parse(x);
+
+            double[] prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(ParseElement)
+                .ToArray();
+
+            Func<double, double> AddVAT = x => x * 1.2;
+            double[] pricesAfterVAT = prices.Select(AddVAT).ToArray();
+
+            Action<double> printPricesAfterVAT = x => Console.WriteLine($"{x:f2}");
+
+            foreach (var price in pricesAfterVAT)
+            {
+                printPricesAfterVAT(price);
+            }
+        }
+    }
+}
