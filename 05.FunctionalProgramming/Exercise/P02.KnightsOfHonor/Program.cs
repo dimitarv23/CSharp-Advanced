4037fed89ee18f4be73b1ea4b80f021702e24b67
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-02 18:17:41 +0300

    Added P02

---
+++ 05.FunctionalProgramming/Exercise/P02.KnightsOfHonor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P02.KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] knights = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> printKnights = k => Console.WriteLine($"Sir {k}");
+
+            foreach (var knight in knights)
+            {
+                printKnights(knight);
+            }
+        }
+    }
+}
