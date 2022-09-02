2ea58458acb6edbe87144eb65440e6f4f0149561
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-02 18:17:35 +0300

    Added P01

---
+++ 05.FunctionalProgramming/Exercise/P01.ActionPrint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> printResult = x => Console.WriteLine(x);
+
+            foreach (var element in input)
+            {
+                printResult(element);
+            }
+        }
+    }
+}
