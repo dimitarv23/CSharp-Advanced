539c96ab6fbb43876b660653b03a0d5007ae54c9
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-02 18:17:47 +0300

    Added P03

---
+++ 05.FunctionalProgramming/Exercise/P03.CustomMinFunction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> getSmallestNum = GetMinNumber;
+            Action<int> printSmallestNum = x => Console.WriteLine(x);
+
+            printSmallestNum(getSmallestNum(numbers));
+        }
+
+        static int GetMinNumber(int[] numbers)
+        {
+            int min = int.MaxValue;
+
+            foreach (var number in numbers)
+            {
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+
+            return min;
+        }
+    }
+}
