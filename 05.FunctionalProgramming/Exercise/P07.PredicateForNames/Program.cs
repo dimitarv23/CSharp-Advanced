c0f5bc3a03f15355df0f18032f8b6453ece13405
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-03 12:04:38 +0300

    Added P07

---
+++ 05.FunctionalProgramming/Exercise/P07.PredicateForNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace P07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, bool> condition = n => n.Length <= length;
+            names = names.Where(condition).ToArray();
+
+            if (names.Length > 0)
+            {
+                foreach (var name in names)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
