c6d0bd48c1daae2c0928e448282cce61c42ba742
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-14 16:12:26 +0300

    Added P01

---
+++ 06.DefiningClasses/Lab/P01.Car/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "BMW";
+            car.Model = "E60 530D";
+            car.Year = 2009;
+
+            Console.WriteLine($"Make: {car.Make}");
+            Console.WriteLine($"Model: {car.Model}");
+            Console.WriteLine($"Year: {car.Year}");
+        }
+    }
+}
