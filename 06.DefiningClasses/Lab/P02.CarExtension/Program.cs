400567270b035bff984b3ce4847760eebbe27b21
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-14 16:12:35 +0300

    Added P02

---
+++ 06.DefiningClasses/Lab/P02.CarExtension/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "BMW";
+            car.Model = "E60 530D";
+            car.Year = 2009;
+            car.FuelQuantity = 50;
+            car.FuelConsumption = 8;
+
+            car.Drive(200);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
