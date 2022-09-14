2266672e25fe8eb63b2760d2782203d8d642bb40
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-14 16:12:48 +0300

    Added P04

---
+++ 06.DefiningClasses/Lab/P04.CarEngineAndTires/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var tires = new Tire[4]
+            {
+                new Tire(1, 2.5),
+                new Tire(1, 2.5),
+                new Tire(2, 2.8),
+                new Tire(2, 2.8)
+            };
+
+            var engine = new Engine(235, 3000);
+
+            var car = new Car("BMW", "E60 530D", 2009, 70, 8, engine, tires);
+        }
+    }
+}
