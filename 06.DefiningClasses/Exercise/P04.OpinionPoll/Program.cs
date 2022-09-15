1b3138eb0eb199550e0e4a490fddcfd495117099
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-15 16:52:59 +0300

    Added P04

---
+++ 06.DefiningClasses/Exercise/P04.OpinionPoll/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                people.Add(new Person(name, age));
+            }
+
+            foreach (var person in people
+                .Where(p => p.Age > 30)
+                .OrderBy(p => p.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
