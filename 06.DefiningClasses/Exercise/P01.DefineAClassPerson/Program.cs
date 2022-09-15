2ca43b1373a002b21e9134d8880fb0bb5a512f3a
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-15 16:52:29 +0300

    Added P01

---
+++ 06.DefiningClasses/Exercise/P01.DefineAClassPerson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //With the empty constructor
+            Person person1 = new Person();
+            person1.Name = "Peter";
+            person1.Age = 20;
+
+            //With the inline initialization constructor
+            Person person2 = new Person("George", 18);
+
+            Person person3 = new Person()
+            {
+                Name = "Jose",
+                Age = 43
+            };
+        }
+    }
+}
