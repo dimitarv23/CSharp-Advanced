03525e5adf2454671a59fd1b988c65d43aa6074f
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-15 16:52:49 +0300

    Added P03

---
+++ 06.DefiningClasses/Exercise/P03.OldestFamilyMember/Family.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+
+        public List<Person> People
+        {
+            get { return people; }
+            set { people = value; }
+        }
+
+        public void AddMembers(Person member)
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldest()
+        {
+            int maxAge = -1;
+            string oldestName = string.Empty;
+
+            foreach (var person in people)
+            {
+                if (person.Age > maxAge)
+                {
+                    maxAge = person.Age;
+                    oldestName = person.Name;
+                }
+            }
+
+            return people.First(p => p.Name == oldestName && p.Age == maxAge);
+        }
+    }
+}
---
+++ 06.DefiningClasses/Exercise/P03.OldestFamilyMember/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currPersonInfo = Console.ReadLine().Split();
+                string personName = currPersonInfo[0];
+                int personAge = int.Parse(currPersonInfo[1]);
+
+                family.AddMembers(new Person(personName, personAge));
+            }
+
+            Person oldestMember = family.GetOldest();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
