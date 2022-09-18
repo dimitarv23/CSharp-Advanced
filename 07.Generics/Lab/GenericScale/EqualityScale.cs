cdb5b878ce5f5eed221db70523cd2c8958f130ea
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-18 13:53:49 +0300

    Added GenericScale

---
+++ 07.Generics/Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        public EqualityScale(T left, T right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+
+        public T Left { get; set; }
+        public T Right { get; set; }
+
+        public bool AreEqual()
+        {
+            if (this.Left.Equals(this.Right))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
