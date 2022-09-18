f9446595988795a3e6abb3b2e9630ff0470d1b9e
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-18 16:52:33 +0300

    Added P02

---
+++ 07.Generics/Exercise/P02.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P02.GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                var box = new Box<int>(input);
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
