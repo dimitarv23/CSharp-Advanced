82d2a21be2cdcdd55faf71c4f65c19c076c1564b
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-25 18:49:45 +0300

    Added P02

---
+++ 08.IteratorsAndComparators/Lab/P02.LibraryIterator/Library.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currIndex;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                Reset();
+                this.books = books.ToList();
+            }
+
+            public Book Current => this.books[this.currIndex];
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                this.currIndex++;
+                return currIndex < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.currIndex = -1;
+            }
+        }
+    }
+}
