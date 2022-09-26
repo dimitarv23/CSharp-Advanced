a30d208238fa173b3429045fba2badac7bc734c1
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-26 10:49:04 +0300

    Added P03

---
+++ 08.IteratorsAndComparators/Exercise/P03.Stack/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace P03.Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> items;
+
+        public Stack()
+        {
+            this.items = new List<T>();
+        }
+
+        public void Push(params T[] values)
+        {
+            foreach (var item in values)
+            {
+                items.Add(item);
+            }
+        }
+        public void Pop()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            items.RemoveAt(items.Count - 1);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = items.Count - 1; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ 08.IteratorsAndComparators/Exercise/P03.Stack/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] cmdArgs = command.Split();
+                string action = cmdArgs[0];
+
+                try
+                {
+                    if (action == "Push")
+                    {
+                        var values = command.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Skip(1).ToArray();
+
+                        stack.Push(values);
+                    }
+                    else if (action == "Pop")
+                    {
+                        stack.Pop();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
