53af0d9ada80dc4c8d4922c4752bea54a2b6f308
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-26 10:47:57 +0300

    Added P03

---
+++ 08.IteratorsAndComparators/Lab/P03.ComparableBook/Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        private string title;
+        private int year;
+        private List<string> authors;
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+
+        public string Title
+        {
+            get { return title; }
+            private set { title = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            private set { year = value; }
+        }
+        public List<string> Authors
+        {
+            get { return authors; }
+            set { authors = value; }
+        }
+
+        public int CompareTo(Book other)
+        {
+            if (this.Year < other.Year)
+            {
+                return -1;
+            }
+            else if (this.Year > other.Year)
+            {
+                return 1;
+            }
+
+            if (this.Title.First() < other.Title.First())
+            {
+                return -1;
+            }
+            else if (this.Title.First() < other.Title.First())
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
---
+++ 08.IteratorsAndComparators/Lab/P03.ComparableBook/Library.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            this.books.Sort();
+
+            for (int i = 0; i < this.books.Count; i++)
+            {
+                yield return this.books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        //private class LibraryIterator : IEnumerator<Book>
+        //{
+        //    private readonly List<Book> books;
+        //    private int currIndex;
+
+        //    public LibraryIterator(IEnumerable<Book> books)
+        //    {
+        //        Reset();
+        //        this.books = books.ToList();
+        //    }
+
+        //    public Book Current => this.books[this.currIndex];
+        //    object IEnumerator.Current => this.Current;
+
+        //    public void Dispose()
+        //    {
+        //    }
+
+        //    public bool MoveNext()
+        //    {
+        //        this.currIndex++;
+        //        return currIndex < this.books.Count;
+        //    }
+
+        //    public void Reset()
+        //    {
+        //        this.currIndex = -1;
+        //    }
+        //}
+    }
+}
