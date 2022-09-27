2eecf55db78e5e05135bf85c5a32fc4d70b383f5
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-27 10:49:32 +0300

    Added P06

---
+++ 08.IteratorsAndComparators/Exercise/P06.EqualityLogic/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace P06.EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Person;
+            return this.Name == other.Name && this.Age == other.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = this.Name.Length * 10000;
+            hashCode = this.Name.Aggregate(hashCode, (current, letter) => current + letter);
+            hashCode += this.Age * 10000;
+
+            return hashCode;
+        }
+    }
+}
---
+++ 08.IteratorsAndComparators/Exercise/P06.EqualityLogic/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06.EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var people = new HashSet<Person>();
+            var sortedPeople = new SortedSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+
+                var person = new Person(personInfo[0], int.Parse(personInfo[1]));
+                people.Add(person);
+                sortedPeople.Add(person);
+            }
+
+            Console.WriteLine(people.Count);
+            Console.WriteLine(sortedPeople.Count);
+        }
+    }
+}
