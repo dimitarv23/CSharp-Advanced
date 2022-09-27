1aba03ed6dae1e6c552d5a52136652f620b997e3
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-27 10:49:19 +0300

    Added P04

---
+++ 08.IteratorsAndComparators/Exercise/P04.Froggy/Lake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+
+        public Lake(params int[] stones)
+        {
+            this.stones = stones.ToList();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.stones.Count; i += 2)
+            {
+                yield return this.stones[i];
+            }
+
+            for (int i = this.stones.Count - 1; i >= 0; i -= 2)
+            {
+                if (i % 2 == 0)
+                {
+                    i++;
+                    continue;
+                }
+
+                yield return this.stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ 08.IteratorsAndComparators/Exercise/P04.Froggy/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace P04.Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake lake = new Lake(input);
+            
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
