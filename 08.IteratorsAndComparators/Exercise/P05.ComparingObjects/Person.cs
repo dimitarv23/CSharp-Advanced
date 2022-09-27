20d7c761011c6e0a94fd5d6a029a8146cde33e4d
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-27 10:49:25 +0300

    Added P05

---
+++ 08.IteratorsAndComparators/Exercise/P05.ComparingObjects/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace P05.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Town
+        {
+            get { return town; }
+            set { town = value; }
+        }
+
+        public int CompareTo(Person other)
+        {
+            var result = this.Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+
+                if (result == 0)
+                {
+                    result = this.Town.CompareTo(other.Town);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 08.IteratorsAndComparators/Exercise/P05.ComparingObjects/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] personInfo = input.Split();
+
+                var person = new Person(personInfo[0], int.Parse(personInfo[1]), personInfo[2]);
+                people.Add(person);
+
+                input = Console.ReadLine();
+            }
+
+            int n = int.Parse(Console.ReadLine());
+            var personToCompare = people[n - 1];
+
+            var equalCount = people.Where(p => p.CompareTo(personToCompare) == 0).Count();
+
+            var unequalCount = people.Where(p => p.CompareTo(personToCompare) != 0).Count();
+
+            if (equalCount > 1)
+            {
+                Console.WriteLine($"{equalCount} {unequalCount} {people.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
