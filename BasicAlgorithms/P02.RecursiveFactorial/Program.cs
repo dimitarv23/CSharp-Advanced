da84e3ff4bb8a8b1417b8f83681236dc3356994a
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-29 17:58:21 +0300

    Added P02

---
+++ BasicAlgorithms/P02.RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P02.RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFactorialRecursively(n));
+        }
+
+        static long GetFactorialRecursively(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * GetFactorialRecursively(n - 1);
+        }
+    }
+}
