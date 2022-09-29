fb1edb38f56e45a8e18b4a57b368831f4a1eca25
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-29 17:58:02 +0300

    Added P01

---
+++ BasicAlgorithms/P01.RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.RecursiveArraySum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(SumRecursively(nums, 0));
+        }
+
+        static int SumRecursively(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                return 0;
+            }
+
+            return arr[index] + SumRecursively(arr, ++index);
+        }
+    }
+}
